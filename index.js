e2610bac5cc97c787e66d17f34005c289ecbb126
Elena Rodriguez <elenarodriguezcalderon@gmail.com>
2023-10-12 10:14:17 +0200

    Añadidas llamadas a los elementos JS

---
+++ index.js
@@ -0,0 +1,4 @@
+const inputEmail = document.getElementById("email");
+const inputPassword = document.getElementById("password");
+const botonLogin = document.getElementById("login");
+const botonRegistro = document.getElementById("registro");
---
+++ registro.js
@@ -0,0 +1,6 @@
+const inputNombre = document.getElementById("nombre");
+const inputApellidos = document.getElementById("apellidos");
+const inputEmail = document.getElementById("email");
+const inputPassword1 = document.getElementById("password1");
+const inputPassword2 = document.getElementById("password2");
+const botonRegistro = document.getElementById("registro");
