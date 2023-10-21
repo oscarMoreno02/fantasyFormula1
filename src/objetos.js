1ad2962050d44de72523a76175df614fc40d5900
Elena Rodriguez <elenarodriguezcalderon@gmail.com>
2023-10-21 11:11:07 +0200

    Creada array de grandesPremios

--- src/objetos.js
+++ src/objetos.js
@@ -33,3 +33,167 @@ export var puntuaciones = [
 	{ posicion: 9, puntuacion: 2 },
 	{ posicion: 10, puntuacion: 1 }
 ];
+
+export var grandesPremios = [{
+	id: 1,
+	nombre: "Gran Premio de Bahréin",
+	ubicacion: "Sakhir",
+	description : "La longitud del circuito es de 5,412 kilómetros. Durante la carrera se dan 57 vueltas al circuito cubriendo una distancia final de 308,484 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 2,
+	nombre: "Gran Premio de Arabia Saudí",
+	ubicacion: "Jeddah",
+	description : "La longitud del circuito es de 6,174 kilómetros. Durante la carrera se darán 50 vueltas al circuito cubriendo una distancia final de 308,700 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 3,
+	nombre: "Gran Premio de Australia",
+	ubicacion: "Melbourne",
+	description : "La longitud del circuito es de 5,303 kilómetros. Durante la carrera se dan 58 vueltas al circuito cubriendo una distancia final de 307,574 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 4,
+	nombre: "Gran Premio de Azerbaiyán",
+	ubicacion: "Bakú",
+	description : ". La longitud del circuito es de 6,003 kilómetros. Durante la carrera se dan 51 vueltas al circuito cubriendo una distancia final de 306,153 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 5,
+	nombre: "Gran Premio de Miami",
+	ubicacion: "Miami",
+	description : "La longitud del circuito es de 5,412 kilómetros. Durante la carrera se dan 57 vueltas al circuito cubriendo una distancia final de 308,484 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 6,
+	nombre: "Gran Premio de Emilia-Romaña",
+	ubicacion: "Imola",
+	description : "La longitud del circuito es de 4,909 kilómetros. Durante la carrera se dan 63 vueltas al circuito cubriendo una distancia final de 309,267 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 7,
+	nombre: "Gran Premio de Mónaco",
+	ubicacion: "Montecarlo",
+	description : "La longitud del circuito es de 3,337 kilómetros. Durante la carrera se dan 78 vueltas al circuito cubriendo una distancia final de 260,286 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 8,
+	nombre: "Gran Premio de España",
+	ubicacion: "Montmeló (Barcelona)",
+	description : "La longitud del circuito es de 4,675 kilómetros. Durante la carrera se dan 66 vueltas al circuito cubriendo una distancia final de 308,550 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 9,
+	nombre: "Gran Premio de Canadá",
+	ubicacion: "Montreal",
+	description : "La longitud del circuito es de 4,361 kilómetros. Durante la carrera se dan 70 vueltas al circuito cubriendo una distancia final de 305,270 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 10,
+	nombre: "Gran Premio de Austria",
+	ubicacion: "Spielberg (Austria)",
+	description : "La longitud del circuito es de 4,318 kilómetros. Durante la carrera se dan 71 vueltas al circuito cubriendo una distancia final de 306,578 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 11,
+	nombre: "Gran Premio de Gran Bretaña",
+	ubicacion: "Silverstone (Reino Unido)",
+	description : "La longitud del circuito es de 5,891 kilómetros. Durante la carrera se dan 52 vueltas al circuito cubriendo una distancia final de 306,332 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 12,
+	nombre: "Gran Premio de Hungría",
+	ubicacion: "Hungaroring (Budapest)",
+	description : "La longitud del circuito es de 4,381 kilómetros. Durante la carrera se dan 70 vueltas al circuito cubriendo una distancia final de 306,670 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 13,
+	nombre: "Gran Premio de Bélgica",
+	ubicacion: "Spa",
+	description : "La longitud del circuito es de 7,004 kilómetros. Durante la carrera se dan 44 vueltas al circuito cubriendo una distancia final de 308,176 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 14,
+	nombre: "Gran Premio de los Países Bajos",
+	ubicacion: "Zandvoort",
+	description : "La longitud del circuito es de 4,259 kilómetros. Durante la carrera se dan 72 vueltas al circuito cubriendo una distancia final de 306,648 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 15,
+	nombre: "Gran Premio de Italia",
+	ubicacion: "Monza",
+	description : "La longitud del circuito es de 5,793 kilómetros. Durante la carrera se dan 53 vueltas al circuito cubriendo una distancia final de 307,029 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 16,
+	nombre: "Gran Premio de Singapur",
+	ubicacion: "Singapur",
+	description : "La longitud del circuito es de 5,063 kilómetros. Durante la carrera se dan 61 vueltas al circuito cubriendo una distancia final de 308,843 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 17,
+	nombre: "Gran Premio de Japón",
+	ubicacion: "Suzuka",
+	description : "La longitud del circuito es de 5,807 kilómetros. Durante la carrera se dan 53 vueltas al circuito cubriendo una distancia final de 307,771 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 18,
+	nombre: "Gran Premio de Qatar",
+	ubicacion: "Lusail",
+	description : "La longitud del circuito es de 5,38 kilómetros. Durante la carrera se dan 57 vueltas al circuito cubriendo una distancia final de 306,660 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 19,
+	nombre: "Gran Premio de Estados Unidos",
+	ubicacion: "Austin (Texas)",
+	description : "La longitud del circuito es de 5,513 kilómetros. Durante la carrera se dan 56 vueltas al circuito cubriendo una distancia final de 308,728 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 20,
+	nombre: "Gran Premio de México",
+	ubicacion: "Ciudad de México",
+	description : "La longitud del circuito es de 4,304 kilómetros. Durante la carrera se dan 71 vueltas al circuito cubriendo una distancia final de 305,584 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 21,
+	nombre: "Gran Premio de Brasil",
+	ubicacion: "São Paulo",
+	description : "La longitud del circuito es de 4,309 kilómetros. Durante la carrera se dan 71 vueltas al circuito cubriendo una distancia final de 305,939 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 22,
+	nombre: "Gran Premio de Las Vegas",
+	ubicacion: "Las Vegas",
+	description : "La longitud del circuito es de 6,12 kilómetros. Durante la carrera se dan 50 vueltas al circuito cubriendo una distancia final de 306,000 kilómetros.",
+	posiciones: "",
+    disputado: false
+},{
+	id: 23,
+	nombre: "Gran Premio de Abu Dabi",
+	ubicacion: "Abu Dabi (Emiratos Árabes)",
+	description : "La longitud del circuito es de 5,281 kilómetros. Durante la carrera se dan 58 vueltas al circuito cubriendo una distancia final de 306,298 kilómetros.",
+	posiciones: "",
+    disputado: false
+}
+]
