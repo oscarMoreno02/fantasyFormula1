a373b5e7e45d9e5dff014da72029230337b9c125
Elena Rodriguez <elenarodriguezcalderon@gmail.com>
2023-10-21 10:46:03 +0200

    Creado array pilotos

---
+++ src/objetos.js
@@ -0,0 +1,22 @@
+export var pilotos = [
+	{ id: 1, nombre: 'George', apellido: 'Russell', nacionalidad: 'Británico', escuderia: 'MERCEDES', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 2, nombre: 'Lewis', apellido: 'Hamilton', nacionalidad: 'Británico', escuderia: 'MERCEDES', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 3, nombre: 'Pierre', apellido: 'Gasly', nacionalidad: 'Francés', escuderia: 'ALPINE', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 4, nombre: 'Esteban', apellido: 'Ocon', nacionalidad: 'Francés', escuderia: 'ALPINE', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 5, nombre: 'Nico', apellido: 'Hulkenberg', nacionalidad: 'Alemán', escuderia: 'HAAS', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 6, nombre: 'Kevin', apellido: 'Magnussen', nacionalidad: 'Danés', escuderia: 'HAAS', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 7, nombre: 'Lando', apellido: 'Norris', nacionalidad: 'Británico', escuderia: 'MCLAREN', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 8, nombre: 'Oscar', apellido: 'Piastri', nacionalidad: 'Australiano', escuderia: 'MCLAREN', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 9, nombre: 'Max', apellido: 'Verstappen', nacionalidad: 'Neerlandés', escuderia: 'RED BULL', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 10, nombre: 'Sergio', apellido: 'Pérez', nacionalidad: 'Mexicano', escuderia: 'RED BULL', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 11, nombre: 'Fernando', apellido: 'Alonso', nacionalidad: 'Español', escuderia: 'ASTON MARTIN', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 12, nombre: 'Lance', apellido: 'Stroll', nacionalidad: 'Canadiense', escuderia: 'ASTON MARTIN', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 13, nombre: 'Yuki', apellido: 'Tsunoda', nacionalidad: 'Japonés', escuderia: 'ALPHATAURI', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 14, nombre: 'Nyck', apellido: 'de Vries', nacionalidad: 'Neerlandés', escuderia: 'ALPHATAURI', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 15, nombre: 'Charles', apellido: 'Leclerc', nacionalidad: 'Monegasco', escuderia: 'FERRARI', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 16, nombre: 'Carlos', apellido: 'Sainz', nacionalidad: 'Español', escuderia: 'FERRARI', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 17, nombre: 'Valtteri', apellido: 'Bottas', nacionalidad: 'Finlandés', escuderia: 'ALFA ROMEO', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 18, nombre: 'Guanyu', apellido: 'Zhou', nacionalidad: 'Chino', escuderia: 'ALFA ROMEO', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 19, nombre: 'Alexander', apellido: 'Albon', nacionalidad: 'Tailandés', escuderia: 'WILLIAMS', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' },
+	{ id: 20, nombre: 'Logan', apellido: 'Sargeant', nacionalidad: 'Estadounidense', escuderia: 'WILLIAMS', puntuacion: 0, propiedadJugador: '', rol: '', srcFoto: '' }
+];
