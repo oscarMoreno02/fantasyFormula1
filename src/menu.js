8ae46f76333715ffb79ea4f2c3b548ce16ad0fb2
Elena Rodriguez <elenarodriguezcalderon@gmail.com>
2023-10-26 16:33:29 +0200

    Creado menú dinámico

---
+++ src/menu.js
@@ -0,0 +1,12 @@
+export function crearMenu() {
+    let div = document.getElementById("menu");
+
+    let nav = document.createElement("nav");
+    nav.innerHTML = "<ul>";
+    "<li><a href='home.html'>Home</a></li>" +
+        "<li><a href='pilotos.html'>Pilotos</a></li>" +
+        "<li><a href='perfil.html'>Perfil</a></li>" +
+        "<li><a href='administracion.html'>Administración</a></li>" +
+        "<li><a href='clasificacion.html'>Clasificación</a></li>" +
+        "</ul>";
+}
