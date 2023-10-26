1a3519b69ee2cf0a2d2a851cc2b71c75139ca2b8
Elena Rodriguez <elenarodriguezcalderon@gmail.com>
2023-10-26 16:34:11 +0200

    Cargado menú en perfil y administracion

--- src/administracion.js
+++ src/administracion.js
@@ -1,37 +1,38 @@
-import {pilotos, puntuaciones, grandesPremios} from './objetos';
+import { pilotos, puntuaciones, grandesPremios } from "./objetos";
+import { crearMenu } from "./menu";
 
 const botonLanzarCarrera = document.getElementById("lanzar-carrera");
 
-botonLanzarCarrera.addEventListener('click', function() {
+botonLanzarCarrera.addEventListener("click", function () {
     disputarGranPremio(grandesPremios);
 });
 
 function disputarGranPremio(grandesPremios) {
-	for (var i = 0; i < grandesPremios.length; i++) {
-		var granPremio = grandesPremios[i];
-		if (!granPremio.disputado) {
-			granPremio.posiciones = generarPosiciones(pilotos);
-			granPremio.disputado = true;
-			break; 
-		}
-	}
+    for (var i = 0; i < grandesPremios.length; i++) {
+        var granPremio = grandesPremios[i];
+        if (!granPremio.disputado) {
+            granPremio.posiciones = generarPosiciones(pilotos);
+            granPremio.disputado = true;
+            break;
+        }
+    }
 }
 
 function generarPosiciones(pilotos) {
-	var posiciones = [];
-	var posicionesAsignadas = new Set();
+    var posiciones = [];
+    var posicionesAsignadas = new Set();
 
-	for (var i = 0; i < pilotos.length; i++) {
-		var piloto = pilotos[i];
-		var posicionAleatoria;
+    for (var i = 0; i < pilotos.length; i++) {
+        var piloto = pilotos[i];
+        var posicionAleatoria;
 
-		do {
-			posicionAleatoria = Math.floor(Math.random() * pilotos.length) + 1;
-		} while (posicionesAsignadas.has(posicionAleatoria));
+        do {
+            posicionAleatoria = Math.floor(Math.random() * pilotos.length) + 1;
+        } while (posicionesAsignadas.has(posicionAleatoria));
 
-		posicionesAsignadas.add(posicionAleatoria);
-		posiciones.push({ idPiloto: piloto.id, posicion: posicionAleatoria });
-	}
+        posicionesAsignadas.add(posicionAleatoria);
+        posiciones.push({ idPiloto: piloto.id, posicion: posicionAleatoria });
+    }
 
-	return posiciones;
+    return posiciones;
 }
