bc69c7e571443ae30735eaca3969cb1ae4485e50
Elena Rodriguez <elenarodriguezcalderon@gmail.com>
2023-11-02 14:12:42 +0100

    Definido asignarPilotos() y calcularPuntuacion()

--- src/js/clases.js
+++ src/js/clases.js
@@ -1,10 +1,45 @@
+import { pilotos } from "./objetos";
+
 export class Usuario {
+    puntuacion = this.calcularPuntuacion();
+
     constructor(nom, ape, email, nick, password) {
         this.nombre = nom;
         this.apellidos = ape;
         this.email = email;
         this.nick = nick;
         this.password = password;
+        this.pilotos = this.asignarPilotos();
+    }
+
+    asignarPilotos() {
+        let i = 0;
+        let pilotos = [];
+
+        do {
+            let idPilotoAleatoria =
+                Math.floor(Math.random() * pilotos.length) + 1;
+
+            for (let piloto in pilotos) {
+                if (piloto.id == idPilotoAleatoria && piloto.rol == "") {
+                    pilotos[i] = piloto;
+
+                    i++;
+                }
+            }
+        } while (i < 0);
+
+        return pilotos;
+    }
+
+    calcularPuntuacion() {
+        let puntos = 0;
+
+        for (let piloto in this.pilotos) {
+            puntos += piloto.puntuacion;
+        }
+
+        return puntos;
     }
 }
 export class Credenciales {
