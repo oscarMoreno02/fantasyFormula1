9ed93af9e57c71fff92316b2ce372605fb82fe1d
Elena Rodriguez <elenarodriguezcalderon@gmail.com>
2023-11-08 22:52:34 +0100

    Finalizado menú superior

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,7 +4,7 @@ export function crearMenu() {
     let nav = document.createElement("nav");
     nav.innerHTML =
         "<ul class='row'>" +
-        "<li class='two columns'><img src='./assets/img/logo.svg'></li>" +
+        "<li class='two columns'><img src='./assets/img/logoBlanco.svg'></li>" +
         "<li class='two columns'><a href='home.html'>Home</a></li>" +
         "<li class='two columns'><a href='pilotos.html'>Pilotos</a></li>" +
         "<li class='two columns'><a href='perfil.html'>Perfil</a></li>" +
